c5f4fe07d56c257e465f4a754cb702ded5d66aa4
Vini Maheshwari <vini.maheshwari@getfareye.com>
2021-05-16 18:35:00 +0000

    testing workflow commented

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,15 +1,15 @@
-package com.example.demo;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-//@SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-		//Assertions.fail("Oops something went wrong");
-	}
-
-}
+//package com.example.demo;
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+////@SpringBootTest
+//class DemoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//		//Assertions.fail("Oops something went wrong");
+//	}
+//
+//}
