a2af3466b87ad99135170359729438523a26a470
Vini Maheshwari <vini.maheshwari@getfareye.com>
2021-05-16 17:49:51 +0000

    let see

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("Oops something went wrong");
 	}
 
 }
